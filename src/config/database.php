13d1192452586caeaedd13a5bb6b81702410b467
unknown <ga_briel@novage.local>
2023-06-06 14:30:32 -0300

    ligação com a base de dados

--- src/config/database.php
+++ src/config/database.php
@@ -1,13 +1,19 @@
 <?php
 
 class Database {
+
     public static function getConnection() {
         $envPath = realpath(dirname(__FILE__) . '/../env.ini');
         $env = parse_ini_file($envPath);
-        $conn = new mysqli($env['host'], $env['username'], $env['database']]);
+        $conn = new mysqli(
+            $env['host'], 
+            $env['username'], 
+            $env['password'], 
+            $env['database']
+        );
 
-        if($conn->conect_error) {
-            die("Erro: " . $conn->conect_error);
+        if($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
         }
 
         return $conn;
