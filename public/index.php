73e98003836af54a489fdd9479fadc21bfc7595b
Gabriel447 <fullstackgabe@gmail.com>
2023-06-12 20:25:05 -0300

    criando metodo getall e fazendo um foreach para renderizar os resultados

--- public/index.php
+++ public/index.php
@@ -3,10 +3,7 @@
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-// echo $user->getSelect();
-
-echo User::getSelect(['id' => 1], 'name, email');
-echo '<br>';
-echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
-
+foreach(User::getAll([], 'name') as $user) {
+    echo $user->name;
+    echo '<br>';
+}
