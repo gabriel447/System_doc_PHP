b0ebbf90663167868094d6befb2b0853cedc034a
Gabriel dos Anjos <fullstackgabe@gmail.com>
2023-06-22 15:14:02 -0300

    criando footer e header para utilizar o modo composite view

---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por Gab<span class="text-danger">ri</span>el</span>
+</footer>
+</body>
+</html>
