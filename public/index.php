262b9f6a8df18146eb4143ff9091ae0c5d8314a6
Gabriel447 <fullstackgabe@gmail.com>
2023-06-25 11:39:14 -0300

    bug de rotas resolvido

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-if($uri === '/' || $uri === '' || $uri === '/index.php' || $uri === '/System_doc_PHP/public/') {
+if($uri === '/' || $uri === '' || $uri === '/index.php') {
     $uri = '/login.php';
 }
 
